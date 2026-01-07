2717975c2ea53b68f9c87d0a4c2bb3192db1a382
rahulg-prog <rahul.g@webknot.in>
2026-01-07 16:48:32 +0530

    everything is working properly

--- Source/exception/custom_exception.py
+++ Source/exception/custom_exception.py
@@ -1,46 +1,43 @@
 import sys
 import traceback
-from typing import Optional, cast
+import os
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    
+    # If running in Jupyter/IPython, try to get a more readable name
+    if 'ipykernel' in file_name or 'IPython' in file_name:
+        # Check if we can get the notebook name from IPython
+        try:
+            from IPython import get_ipython
+            ipython = get_ipython()
+            if ipython and hasattr(ipython, 'user_ns'):
+                # Try to get notebook name from the session
+                if '__vsc_ipynb_file__' in ipython.user_ns:
+                    file_name = ipython.user_ns['__vsc_ipynb_file__']
+                else:
+                    file_name = "Jupyter Notebook"
+        except:
+            file_name = "Jupyter Notebook"
+    
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
+    
+    return error_message
 
 class DocumentPortalException(Exception):
-    def __init__(self, error_message, error_details: Optional[object] = None):
-        if isinstance(error_message, BaseException):
-            norm_msg = str(error_message)
-        else:
-            norm_msg = str(error_message)
-
-        exc_type = exc_value = exc_tb = None
-        if error_details is None:
-            exc_type, exc_value, exc_tb = sys.exc_info()
-        else:
-            if hasattr(error_details, "exc_info"):
-                exc_info_obj = cast(sys, error_details)
-                exc_type, exc_value, exc_tb = exc_info_obj.exc_info()
-            elif isinstance(error_details, BaseException):
-                exc_type, exc_value, exc_tb = type(error_details), error_details, error_details.__traceback__
-            else:
-                exc_type, exc_value, exc_tb = sys.exc_info()
-
-        last_tb = exc_tb
-        while last_tb and last_tb.tb_next:
-            last_tb = last_tb.tb_next
-
-        self.file_name = last_tb.tb_frame.f_code.co_filename if last_tb else "<unknown>"
-        self.lineno = last_tb.tb_lineno if last_tb else -1
-        self.error_message = norm_msg
-
-        if exc_type and exc_tb:
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        
+        # Store full traceback for logging
+        _, _, exc_tb = error_detail.exc_info()
+        if exc_tb:
+            exc_type, exc_value, _ = error_detail.exc_info()
             self.traceback_str = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
         else:
             self.traceback_str = ""
-
-        super().__init__(self.__str__())
-
+    
     def __str__(self):
-        base = f"Error in [{self.file_name}] at line [{self.lineno}] | Message: {self.error_message}"
-        if self.traceback_str:
-            return f"{base}\nTraceback:\n{self.traceback_str}"
-        return base
-
-    def __repr__(self):
-        return f"DocumentPortalException(file={self.file_name!r}, line={self.lineno}, message={self.error_message!r})"
+        return self.error_message
